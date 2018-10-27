8db83ce3303c3ea2756e4468767617d301873957
Nighko <barney.bruckner@gmail.com>
2018-10-27 12:28:15 +1100

    MAJOR UPDATE
    Kewl updates

---
+++ controller.py
@@ -0,0 +1,26 @@
+# Import custom functions
+from voice_engine import ve, calib
+from tts import tts_engine as ttse
+from internet_test import ping
+# Import installed libraries
+import csv
+# Check for internet connection. Use WIT.AI if you have one and use
+# PocketSphinx if offline.
+internet_on = ping()
+with open('preferences.csv') as  csvfile:
+    readCSV = csv.reader(csvfile, delimiter = ',')
+    for row in readCSV:
+        g = row[0]
+calib()
+ttse(g, "Bright is online")
+while(True):
+    ttse(g, "Awaiting user input")
+    print("Awaiting user input")
+    recog = ve(False, internet_on)
+    ttse(g, "Recognised: " + recog)
+    print("Recognised: " + recog)
+    #if hotword == "hey illuminate" :
+    #    print("Hotword Detected")
+    #else:
+    #    print("Not Hotword D: it thinks you said: " + hotword)
+input()
---
+++ voice_engine.py
@@ -0,0 +1,28 @@
+# Voice Engine class.
+def calib():
+    import speech_recognition as sr
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+
+def ve(dummy_engine, internet_on):
+    import speech_recognition as sr
+    r = sr.Recognizer()
+    # Check for internet connection (If connected: Wit.AI/If not connected: PocketSphinx)
+    # Get Audio
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        if internet_on == True:
+            # Use Wit AI
+            WIT_AI_KEY = "E7EQGHO4L2ZLAJCZF32UGNN3WA5ILCBW"
+            try:
+                user_input = r.recognize_wit(audio, key=WIT_AI_KEY)
+            except sr.UnknownValueError:
+                user_input= 0
+        else:
+                # Use PocketSphinx
+            try:
+                user_input=r.recognize_sphinx(audio)
+            except sr.UnknownValueError:
+                user_input= 0
+    return user_input
